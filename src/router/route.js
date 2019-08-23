0eccf103d4c10ce0146015ae79b689719fcfd5e0
bertin <ruhimbazab@gmail.com>
2019-08-23 21:11:51 +0200

    feat(route): add signin route [finishes #167848798]

--- src/router/route.js
+++ src/router/route.js
@@ -6,5 +6,6 @@ import userController from '../controller/userController';
 const route = express.Router();
 
 route.post('/api/v1/auth/signUp', userController.signUp);
+route.post('/api/v1/auth/signIn', userController.signIn);
 
 export default route;
