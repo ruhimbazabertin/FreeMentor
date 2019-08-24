d30cf121a27137e1a6f37e9e7a32d4964f091b59
bertin <ruhimbazab@gmail.com>
2019-08-24 18:50:14 +0200

    feat(create session route): added route for creating a request

--- src/router/route.js
+++ src/router/route.js
@@ -1,6 +1,7 @@
 
 import express from 'express';
 import userController from '../controller/userController';
+import sessionController from '../controller/sessionController';
 import auth from '../middleware/auth';
 
 
@@ -11,5 +12,6 @@ route.post('/api/v1/auth/signIn', userController.signIn);
 route.get('/api/v1/auth/mentors/:id', auth, userController.specificMentor);
 route.patch('/api/v1/auth/user/:id', auth, userController.changeToMentor);
 route.get('/api/v1/auth/mentors', auth, userController.viewMentors);
+route.post('/api/v1/auth/sessions', auth, sessionController.createSession);
 
 export default route;
