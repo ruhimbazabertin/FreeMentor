39fc03fb569971473418c0966d85a83ac6181371
bertin <ruhimbazab@gmail.com>
2019-08-24 15:21:47 +0200

    feat(route): added route to change a user to a mentor [finishes #167849485]

--- src/router/route.js
+++ src/router/route.js
@@ -9,5 +9,6 @@ const route = express.Router();
 route.post('/api/v1/auth/signUp', userController.signUp);
 route.post('/api/v1/auth/signIn', userController.signIn);
 route.get('/api/v1/auth/mentors/:id', auth, userController.specificMentor);
+route.patch('/api/v1/auth/user/:id', auth, userController.changeToMentor);
 
 export default route;
