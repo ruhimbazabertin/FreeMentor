2a6c5a3fcf04c08b290b2ca7e0b58d44e7ffb67f
bertin <ruhimbazab@gmail.com>
2019-08-25 20:55:03 +0200

    fix coverage issue

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -39,108 +39,111 @@ class userController {
       data: newUser.value,
     });
   }
-   //SignIn
-   static signIn(req,res){
-        
-    const { email, password} = req.body;
-     const foundUser = userModel.find(user => user.email === email);
-     if(foundUser){
-         const verifyPassword = bcryptjs.compareSync(req.body.password, foundUser.password);
-         if(verifyPassword){
-             const jwToken = jwt.sign({ id: foundUser.id, firstName: foundUser.firstName, lastName: foundUser.lastName, email:foundUser.email, address:foundUser.address, bio:foundUser.bio, occupation:foundUser.occupation, expertise:foundUser.expertise, userType:foundUser.userType, }, process.env.SECRET_KEY, {expiresIn: '1 day'});
-            return res.status(200).json({
-                status: 200,
-                message: `Logged in as ${foundUser.firstName}`,
-                data: {
-                    firstName:foundUser.firstName,lastName:foundUser.lastName,email:foundUser.email, address:foundUser.address,bio:foundUser.bio,occupation:foundUser.occupation,expertise:foundUser.expertise,userType:foundUser.userType
-                },
-               token: jwToken          
-            });
-         }
-         return res.status(404).json({
-            status: 404,
-            error: 'UserName or password not match.'
+
+  // SignIn
+  static signIn(req, res) {
+    const { email, password } = req.body;
+    const foundUser = userModel.find(user => user.email === email);
+    if (foundUser) {
+      const verifyPassword = bcryptjs.compareSync(req.body.password, foundUser.password);
+      if (verifyPassword) {
+        const jwToken = jwt.sign({
+          id: foundUser.id, firstName: foundUser.firstName, lastName: foundUser.lastName, email: foundUser.email, address: foundUser.address, bio: foundUser.bio, occupation: foundUser.occupation, expertise: foundUser.expertise, userType: foundUser.userType,
+        }, process.env.SECRET_KEY);
+        return res.status(200).json({
+          status: 200,
+          message: `Logged in as ${foundUser.firstName}`,
+          data: {
+            firstName: foundUser.firstName, lastName: foundUser.lastName, email: foundUser.email, address: foundUser.address, bio: foundUser.bio, occupation: foundUser.occupation, expertise: foundUser.expertise, userType: foundUser.userType,
+          },
+          token: jwToken,
         });
-    }
-    return res.status(404).json({
-        status: 404,
-        error: 'User not found'
-    });
- }
-     //View a specific mentor
-      static specificMentor(req, res){
-        if(req.user.userType === 'user'){
-        const { id } = req.params;
-        const foundUser = userModel.find(user => user.id === parseInt(id) && user.userType === 'mentor' );
-        if(foundUser){
-           return res.status(200).json({
-            status: 200,
-            data: foundUser
-             
-           })
-        }
-        return res.status(404).json({
-            status: 404,
-            error: 'Mentor not found'
-        });
-    }
-    return res.status(403).json({
-        status: 403,
-        error: 'Unauthorized'
-    });
-     }  
-     //Change user to mentor
-     static changeToMentor(req, res){
-        if(req.user.userType === 'admin'){
-           
-       const { id } = req.params;
-      const findUser = userModel.find(user => user.id === parseInt(id) && user.userType === 'user');
-      if(findUser){
-          const updateUser={
-           id: findUser.id,
-           firstName: findUser.firstName,
-           lastName: findUser.lastName,
-           email: findUser.email,
-           passsword: findUser.password,
-           address: findUser.address,
-           bio: findUser.bio,
-           occupation: findUser.occupation,
-           expertise: findUser.expertise,
-           userType: 'mentor'
-          };
-          
-          userModel[userModel.indexOf(findUser)] = updateUser;
-          return res.status(200).json({
-              status: 200,
-              data: updateUser,
-              
-          })
       }
       return res.status(404).json({
-       status: 404,
-       error: 'User not found or may be is a mentor'
-   });
-  }
- return res.status(403).json({
- status: 403,
- message: 'Unauthorize'
-});
+        status: 404,
+        error: 'UserName or password not match.',
+      });
     }
-  //all mentors
-  static viewMentors(req, res){
-  if(req.user.userType === 'user' || req.user.userType === 'admin'){
-  const allMenotrs = userModel.filter(mentor => mentor.userType === 'mentor'); 
-     return res.status(200).json({
+    return res.status(404).json({
+      status: 404,
+      error: 'User not found',
+    });
+  }
+
+  // View a specific mentor
+  static specificMentor(req, res) {
+    if (req.user.userType === 'user') {
+      const { id } = req.params;
+      const foundUser = userModel.find(user => user.id === parseInt(id) && user.userType === 'mentor');
+      if (foundUser) {
+        return res.status(200).json({
           status: 200,
-          data: allMenotrs,           
-        })  
-        }
-        return res.status('403').json({
-            status: 403,
-            message: 'Aunauthorized'
-        })
-       }
-   
+          data: foundUser,
+
+        });
+      }
+      return res.status(404).json({
+        status: 404,
+        error: 'Mentor not found',
+      });
+    }
+    return res.status(403).json({
+      status: 403,
+      error: 'Unauthorized',
+    });
+  }
+
+  // Change user to mentor
+  static changeToMentor(req, res) {
+    if (req.user.userType === 'admin') {
+      const { id } = req.params;
+      const findUser = userModel.find(user => user.id === parseInt(id) && user.userType === 'user');
+      if (findUser) {
+        const updateUser = {
+          id: findUser.id,
+          firstName: findUser.firstName,
+          lastName: findUser.lastName,
+          email: findUser.email,
+          passsword: findUser.password,
+          address: findUser.address,
+          bio: findUser.bio,
+          occupation: findUser.occupation,
+          expertise: findUser.expertise,
+          userType: 'mentor',
+        };
+
+        userModel[userModel.indexOf(findUser)] = updateUser;
+        return res.status(200).json({
+          status: 200,
+          data: updateUser,
+
+        });
+      }
+      return res.status(404).json({
+        status: 404,
+        error: 'User not found or may be is a mentor',
+      });
+    }
+    return res.status(403).json({
+      status: 403,
+      message: 'Unauthorize',
+    });
+  }
+
+  // all mentors
+  static viewMentors(req, res) {
+    if (req.user.userType === 'user' || req.user.userType === 'admin') {
+      const allMenotrs = userModel.filter(mentor => mentor.userType === 'mentor');
+      return res.status(200).json({
+        status: 200,
+        data: allMenotrs,
+      });
+    }
+    return res.status('403').json({
+      status: 403,
+      message: 'Aunauthorized',
+    });
+  }
 }
 
 export default userController;
