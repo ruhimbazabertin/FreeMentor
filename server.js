e3a8a45164dcdd46263dfec6dcf9e9fd3cd952e6
bertin <ruhimbazab@gmail.com>
2019-09-01 18:35:54 +0200

    fixing folder structure issue

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 app.use(route);
-// app.use(route);
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`server is running on port: ${port}`));
